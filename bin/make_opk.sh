b88a1f44ccfd6c5a2c4af087b6aceff0219bbd19
goldmojo <goldmojo@free.fr>
2020-10-04 11:45:35 +0200

    Create a partial OPK with FBA UI only

--- bin/make_opk.sh
+++ bin/make_opk.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-rm -f fba-rg350.opk
+rm -f fba.opk
+rm -f fba_ui_only.opk
 
-mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_explorer.gcw0.desktop aliases.txt fba-rg350.opk -all-root -no-xattrs -noappend -no-exports
+mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_explorer.gcw0.desktop aliases.txt fba.opk -all-root -no-xattrs -noappend -no-exports
+mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_ui_only.opk -all-root -no-xattrs -noappend -no-exports
