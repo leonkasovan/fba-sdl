7ad3dd99ccdf1f3fa3ea5d357f5f3dfda648a3cf
Pedro Luis Rodríguez González <pl.rguez@gmail.com>
2021-07-28 02:01:16 +0200

    Fix reading zipped roms in OpenDingux Lepus

--- src/sdl-dingux/bzip.cpp
+++ src/sdl-dingux/bzip.cpp
@@ -371,7 +371,11 @@ int BzipOpen(bool bootApp)
 			free(szBzipName[z]);
 			szBzipName[z] = (TCHAR*)malloc(MAX_PATH * sizeof(TCHAR));
 
+#if OD_LEPUS
+			_stprintf(szBzipName[z], _T("%s%s"), szAppRomPaths[d], szName);
+#else
 			_stprintf(szBzipName[z], _T("%s%hs"), szAppRomPaths[d], szName);
+#endif
 
 			if (ZipOpen(TCHARToANSI(szBzipName[z], NULL, 0)) == 0) {	// Open the rom zip file
 				nZipsFound++;
