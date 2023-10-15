2d4212b9bd8ac1a9a15570acf4251aabe58ca2fa
Novan using Ubuntu WSL PC <dhani.novan@gmail.com>
2023-10-15 16:05:35 +0700

    change dingux to rg35xx 	deleted:    bin/fba_explorer.gcw0.desktop 	deleted:    bin/fba_explorer.lepus.desktop 	deleted:    bin/fba_ux.gcw0.desktop 	deleted:    bin/fba_ux.lepus.desktop 	deleted:    bin/make_opk.sh 	deleted:    build_odbeta.sh 	deleted:    src/sdl-dingux/ao_audio.cpp 	deleted:    src/sdl-dingux/ao_audio.h 	deleted:    src/sdl-dingux/burner.h 	deleted:    src/sdl-dingux/bzip.cpp 	deleted:    src/sdl-dingux/config.cpp 	deleted:    src/sdl-dingux/dat.cpp 	deleted:    src/sdl-dingux/drv.cpp 	deleted:    src/sdl-dingux/font.cpp 	deleted:    src/sdl-dingux/font.h 	deleted:    src/sdl-dingux/gameinp.h 	deleted:    src/sdl-dingux/gui_config.cpp 	deleted:    src/sdl-dingux/gui_config.h 	deleted:    src/sdl-dingux/gui_gfx.cpp 	deleted:    src/sdl-dingux/gui_gfx.h 	deleted:    src/sdl-dingux/gui_language_pack.cpp 	deleted:    src/sdl-dingux/gui_language_pack.h 	deleted:    src/sdl-dingux/gui_main.cpp 	deleted:    src/sdl-dingux/gui_main.h 	deleted:    src/sdl-dingux/gui_romlist.cpp 	deleted:    src/sdl-dingux/gui_romlist.h 	deleted:    src/sdl-dingux/gui_setpath.cpp 	deleted:    src/sdl-dingux/gui_setpath.h 	deleted:    src/sdl-dingux/input.cpp 	deleted:    src/sdl-dingux/ioapi.c 	deleted:    src/sdl-dingux/ioapi.h 	deleted:    src/sdl-dingux/main.cpp 	deleted:    src/sdl-dingux/neocdlist.cpp 	deleted:    src/sdl-dingux/neocdlist.h 	deleted:    src/sdl-dingux/paths.cpp 	deleted:    src/sdl-dingux/run.cpp 	deleted:    src/sdl-dingux/sdl_audio.cpp 	deleted:    src/sdl-dingux/sdl_audio.h 	deleted:    src/sdl-dingux/sdl_input.cpp 	deleted:    src/sdl-dingux/sdl_input.h 	deleted:    src/sdl-dingux/sdl_menu.cpp 	deleted:    src/sdl-dingux/sdl_menu.h 	deleted:    src/sdl-dingux/sdl_progress.cpp 	deleted:    src/sdl-dingux/sdl_progress.h 	deleted:    src/sdl-dingux/sdl_run.cpp 	deleted:    src/sdl-dingux/sdl_run.h 	deleted:    src/sdl-dingux/sdl_video.cpp 	deleted:    src/sdl-dingux/sdl_video.h 	deleted:    src/sdl-dingux/skin/fba_bg.png 	deleted:    src/sdl-dingux/skin/fba_font.png 	deleted:    src/sdl-dingux/skin/fba_selector.png 	deleted:    src/sdl-dingux/skin/fba_title.png 	deleted:    src/sdl-dingux/snd.cpp 	deleted:    src/sdl-dingux/snd.h 	deleted:    src/sdl-dingux/state.cpp 	deleted:    src/sdl-dingux/stringset.cpp 	deleted:    src/sdl-dingux/stubs.cpp 	deleted:    src/sdl-dingux/tchar.cpp 	deleted:    src/sdl-dingux/tchar.h 	deleted:    src/sdl-dingux/unzip.c 	deleted:    src/sdl-dingux/unzip.h 	deleted:    src/sdl-dingux/zipfn.cpp
    	Makefile.rg35xx
    	build.sh
    	export_bin.sh
    	src/sdl-rg35xx/

--- bin/make_opk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -f fba.opk
-rm -f fba_ui_only.opk
-
-mksquashfs skin fbasdl_gcw0.dge fbasdl_lepus.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_ux.lepus.desktop fba_explorer.gcw0.desktop fba_explorer.lepus.desktop aliases.txt fba.opk -all-root -no-xattrs -noappend -no-exports
-mksquashfs skin fbasdl_gcw0.dge fbasdl_lepus.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_ux.lepus.desktop fba_ui_only.opk -all-root -no-xattrs -noappend -no-exports
